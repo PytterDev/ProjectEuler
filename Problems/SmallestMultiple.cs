f5fd3f0b4bed5ad367521f4e9833bc1e218ecfc2
Pytter <pytter.dev@gmail.com>
2025-01-05 19:12:15 -0500

    Smallest Multiple

---
+++ Problems/SmallestMultiple.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using ProjectEuler.Helpers;
+
+namespace ProjectEuler.Problems
+{
+    internal class SmallestMultiple
+    {
+        public static int Solve()
+        {
+            int result = 0;
+            int initNumber = 2520;
+            int count;
+
+            while (true)
+            {
+                count = 0;
+                while (count <= 20)
+                {
+                    count++;
+                    if (!MathHelper.IsModel(initNumber, count))
+                    {
+                        break;
+                    }
+                }
+
+                count = count - 1;
+
+                if (count == 20)
+                {
+                    result = initNumber;
+                    break;
+                }
+
+                initNumber++;
+            }
+
+            return result;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -14,7 +14,8 @@ namespace ProjectEuler
             //Console.WriteLine("Multiples of 3 or 5: " + MultiplesThreeFive.Solve(1000, [3, 5]));
             //Console.WriteLine("Even Fibonacci Numbers: " + EvenFibonacciNumbers.Solve(4000000));
             //Console.WriteLine("Largest Prime Factor: " + LargestPrimeFactor.Solve(600851475143));
-            Console.WriteLine("Largest Palindrome Product: " + LargestPalindromeProduct.Solve());
+            //Console.WriteLine("Largest Palindrome Product: " + LargestPalindromeProduct.Solve());
+            Console.WriteLine("Largest Palindrome Product: " + SmallestMultiple.Solve());
         }
     }
 }
